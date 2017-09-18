d290ff9aa506f0feafad659583d808d29c5be0df
rvanschooten <rvanschooten@unic-utrecht.nl>
2017-09-18 10:28:52 +0200

    score?

--- Clicker Game/sketch.js
+++ Clicker Game/sketch.js
@@ -1,3 +1,5 @@
+var score = 0;
+
 function setup() {
 createCanvas(windowWidth, windowHeight);
 }
@@ -6,9 +8,11 @@ function draw() {
   background(255)
   if (mouseIsPressed)
     ellipse(width/2, height/2, 100, 100);
+    score++;
   else
     rect(width/2-50, height/2-50, 100, 100);
 
   print(mouseIsPressed);
-
+  textSize(200);
+  text(score, width/2, 150);
 }
