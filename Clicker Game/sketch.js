9ed9fd7c1cf8a4f576a414a8f7f6690a774379e4
rvanschooten <rvanschooten@unic-utrecht.nl>
2017-09-18 10:14:48 +0200

    mouseIsPressed

--- Clicker Game/sketch.js
+++ Clicker Game/sketch.js
@@ -4,4 +4,11 @@ createCanvas(windowWidth, windowHeight);
 
 function draw() {
   ellipse(width/2, height/2, 80, 100);
+  if (mouseIsPressed)
+    ellipse(50, 50, 50, 50);
+  else
+    rect(25, 25, 50, 50);
+
+  print(mouseIsPressed);
+  }
 }
